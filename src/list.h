3af27426aac8befaf20c1f969dbeb974d1cf06c8
coord.e <me@coord-e.com>
2019-07-09 00:56:01 +0000

    Fix: Fix generated list functions to have internal linkage

--- src/list.h
+++ src/list.h
@@ -10,21 +10,21 @@
     T head;                                     \
     Name* tail;                                 \
   };                                            \
-  Name* init_##Name() {                         \
+  static Name* init_##Name() {                         \
     return NULL;                                \
   }                                             \
-  Name* single_##Name(T value) {                \
+  static Name* single_##Name(T value) {                \
     Name* l = malloc(sizeof(Name));             \
     l->head = value;                            \
     l->tail = NULL;                             \
     return l;                                   \
   }                                             \
-  Name* cons_##Name(T value, Name* list) {      \
+  static Name* cons_##Name(T value, Name* list) {      \
     Name* l = single_##Name(value);             \
     l->tail = list;                             \
     return l;                                   \
   }                                             \
-  Name* append_##Name(T value, Name* list) {    \
+  static Name* append_##Name(T value, Name* list) {    \
     if(list->tail == NULL) {                    \
       Name* l = single_##Name(value);           \
       list->tail = l;                           \
@@ -35,7 +35,7 @@
   }
 
 #define DEFINE_LIST_PRINTER(print_data, Name)                          \
-  void p_print_##Name(FILE* f, Name* l) {                         \
+  static void p_print_##Name(FILE* f, Name* l) {                         \
     print_data(f, l->head);                                       \
     if (l->tail == NULL) {                                        \
       return;                                                     \
@@ -43,6 +43,6 @@
       p_print_##Name(f, l->tail);                                 \
     }                                                             \
   }                                                               \
-  void print_##Name(FILE* f, Name* l) { p_print_##Name(f, l); fprintf(f, "\n"); }
+  static void print_##Name(FILE* f, Name* l) { p_print_##Name(f, l); fprintf(f, "\n"); }
 
 #endif

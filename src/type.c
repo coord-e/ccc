91990d457beac38397fd346808f6e9bbb84ee29f
coord.e <me@coord-e.com>
2019-08-01 12:49:03 +0000

    Add: Add some utility in `type`

--- src/type.c
+++ src/type.c
@@ -1,4 +1,5 @@
 #include "type.h"
+#include "error.h"
 
 #include <stdlib.h>
 
@@ -16,4 +17,43 @@ void release_Type(Type* ty) {
 
   release_Type(ty->ptr_to);
   free(ty);
+}
+
+void print_Type(FILE* p, Type* ty) {
+  switch (ty->kind) {
+    case TY_INT:
+      fprintf(p, "int");
+      break;
+    case TY_PTR:
+      print_Type(p, ty->ptr_to);
+      fprintf(p, "*");
+      break;
+    default:
+      CCC_UNREACHABLE;
+  }
+}
+
+bool equal_to_Type(const Type* a, const Type* b) {
+  if (a->kind != b->kind) {
+    return false;
+  }
+
+  switch (a->kind) {
+    case TY_INT:
+      return true;
+    case TY_PTR:
+      return equal_to_Type(a->ptr_to, b->ptr_to);
+    default:
+      CCC_UNREACHABLE;
+  }
+}
+
+Type* int_ty() {
+  return new_Type(TY_INT);
+}
+
+Type* ptr_to_ty(Type* ty) {
+  Type* t   = new_Type(TY_PTR);
+  t->ptr_to = ty;
+  return t;
 }
--- src/type.h
+++ src/type.h
@@ -1,6 +1,9 @@
 #ifndef CCC_TYPE_H
 #define CCC_TYPE_H
 
+#include <stdbool.h>
+#include <stdio.h>
+
 typedef enum {
   TY_INT,
   TY_PTR,
@@ -15,5 +18,10 @@ struct Type {
 
 Type* new_Type(TypeKind);
 void release_Type(Type*);
+void print_Type(FILE*, Type*);
+bool equal_to_Type(const Type*, const Type*);
+
+Type* int_ty();
+Type* ptr_to_ty(Type*);
 
 #endif

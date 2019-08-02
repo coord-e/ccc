4eac31828ebad7dedf00e88f10659846c82a2e94
coord.e <me@coord-e.com>
2019-08-02 13:13:42 +0000

    Add: Implement sematic analysis of expressions

--- src/sema.c
+++ src/sema.c
@@ -24,6 +24,14 @@ static void add_var(Env* env, const char* name, Type* ty) {
   insert_TypeMap(env->vars, name, ty);
 }
 
+static Type* get_var(Env* env, const char* name) {
+  Type* ty;
+  if (!lookup_TypeMap(env->vars, name, &ty)) {
+    error("undeclared identifier \"%s\"", name);
+  }
+  return ty;
+}
+
 static Type* ptrify(Type* base, unsigned num) {
   if (num == 0) {
     return base;
@@ -32,7 +40,81 @@ static Type* ptrify(Type* base, unsigned num) {
   }
 }
 
-static Type* sema_expr(Env* env, Expr* expr);
+static noreturn void type_error(Type* expected, Type* got) {
+  fputs("expected: ", stderr);
+  print_Type(stderr, expected);
+  fputs("but got: ", stderr);
+  print_Type(stderr, got);
+  fputs("\n", stderr);
+  error("type mismatch");
+}
+
+static void should_equal(Type* expected, Type* ty) {
+  if (!equal_to_Type(expected, ty)) {
+    type_error(expected, ty);
+  }
+}
+
+// returned `Type*` is reference to a data is owned by `expr`
+static Type* sema_expr(Env* env, Expr* expr) {
+  Type* t;
+  switch (expr->kind) {
+    case ND_BINOP: {
+      Type* lhs_ty = sema_expr(env, expr->lhs);
+      Type* rhs_ty = sema_expr(env, expr->rhs);
+      t            = sema_binop(expr->binop, lhs_ty, rhs_ty);
+      break;
+    }
+    case ND_ASSIGN: {
+      Type* lhs_ty = sema_expr(env, expr->lhs);
+      Type* rhs_ty = sema_expr(env, expr->rhs);
+      // TODO: type conversion
+      should_equal(lhs_ty, rhs_ty);
+      t = copy_Type(lhs_ty);
+      break;
+    }
+    case ND_VAR:
+      t = get_var(env, expr->var);
+      break;
+    case ND_NUM:
+      t = int_ty();
+      break;
+    case ND_CALL: {
+      Type* lhs_ty = sema_expr(env, expr->lhs);
+      if (lhs_ty->kind != TY_PTR || lhs_ty->ptr_to->kind != TY_FUNC) {
+        fputs("attempt to call a value with type ", stderr);
+        print_Type(stderr, lhs_ty);
+        fputs("\n", stderr);
+        error("could not call a value other than function pointers");
+      }
+
+      Type* f             = lhs_ty->ptr_to;
+      unsigned num_args   = length_ExprVec(expr->args);
+      unsigned num_params = length_TypeVec(f->params);
+      if (num_args != num_params) {
+        error("too many / too few arguments to function");
+      }
+
+      for (unsigned i = 0; i < num_args; i++) {
+        Expr* a    = get_ExprVec(expr->args, i);
+        Type* a_ty = sema_expr(env, a);
+        Type* p_ty = get_TypeVec(f->params, i);
+
+        if (!equal_to_Type(a_ty, p_ty)) {
+          type_error(a_ty, p_ty);
+        }
+      }
+
+      t = copy_Type(f->ret);
+      break;
+    }
+    default:
+      CCC_UNREACHABLE;
+  }
+  expr->type = t;
+  return t;
+}
+
 static void sema_decl(Env* env, Declaration* decl);
 
 static void sema_items(Env* env, BlockItemList* l);

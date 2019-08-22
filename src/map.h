15219db8830d7d5ef3d03a998146fd2aa506a0ec
coord.e <me@coord-e.com>
2019-08-22 07:59:25 +0000

    Change: Use pointers in Map

--- src/map.h
+++ src/map.h
@@ -9,9 +9,6 @@
 
 // hash table from string to `T`
 #define DECLARE_MAP(T, Name)                                                                       \
-  typedef struct Name##Entry Name##Entry;                                                          \
-  DECLARE_LIST(Name##Entry, Name##Entries)                                                         \
-  DECLARE_VECTOR(Name##Entries*, Name##Table)                                                      \
   typedef struct Name Name;                                                                        \
   Name* new_##Name(unsigned size);                                                                 \
   Name* copy_##Name(const Name*);                                                                  \
@@ -23,16 +20,22 @@
   void release_##Name(Name*);
 
 #define DEFINE_MAP(copy_T, release_T, T, Name)                                                     \
-  struct Name##Entry {                                                                             \
+  typedef struct {                                                                                 \
     unsigned hash;                                                                                 \
     char* key;                                                                                     \
     T value;                                                                                       \
-  };                                                                                               \
-  static void release_##Name##Entry(Name##Entry e) {                                               \
-    free(e.key);                                                                                   \
-    release_T(e.value);                                                                            \
+  } Name##Entry;                                                                                   \
+  static void release_##Name##Entry(Name##Entry* e) {                                              \
+    if (e == NULL) {                                                                               \
+      return;                                                                                      \
+    }                                                                                              \
+    free(e->key);                                                                                  \
+    release_T(e->value);                                                                           \
+    free(e);                                                                                       \
   }                                                                                                \
-  DEFINE_LIST(release_##Name##Entry, Name##Entry, Name##Entries)                                   \
+  DECLARE_LIST(Name##Entry*, Name##Entries)                                                        \
+  DECLARE_VECTOR(Name##Entries*, Name##Table)                                                      \
+  DEFINE_LIST(release_##Name##Entry, Name##Entry*, Name##Entries)                                  \
   DEFINE_VECTOR(release_##Name##Entries, Name##Entries*, Name##Table)                              \
   struct Name {                                                                                    \
     Name##Table* table;                                                                            \
@@ -45,18 +48,26 @@
     }                                                                                              \
     return m;                                                                                      \
   }                                                                                                \
-  Name##Entries* copy_entries_##Name(const Name##Entries* list, bool is_deep) {                    \
+  static Name##Entry* new_##Name##Entry() { return calloc(1, sizeof(Name##Entry)); }               \
+  static Name##Entry* copy_##Name##Entry(Name##Entry* e, bool copy_value) {                        \
+    Name##Entry* new = new_##Name##Entry();                                                        \
+    new->hash        = e->hash;                                                                    \
+    new->key         = strdup(e->key);                                                             \
+    if (copy_value) {                                                                              \
+      new->value = copy_T(e->value);                                                               \
+    } else {                                                                                       \
+      new->value = e->value;                                                                       \
+    }                                                                                              \
+    return new;                                                                                    \
+  }                                                                                                \
+  static Name##Entries* copy_entries_##Name(const Name##Entries* list, bool copy_value) {          \
     if (list->is_nil) {                                                                            \
       return nil_##Name##Entries();                                                                \
     }                                                                                              \
-    Name##Entry e = list->head;                                                                    \
-    e.key         = strdup(e.key);                                                                 \
-    if (is_deep) {                                                                                 \
-      e.value = copy_T(e.value);                                                                   \
-    }                                                                                              \
-    return cons_##Name##Entries(e, copy_entries_##Name(list->tail, is_deep));                      \
+    Name##Entry* e = copy_##Name##Entry(list->head, copy_value);                                   \
+    return cons_##Name##Entries(e, copy_entries_##Name(list->tail, copy_value));                   \
   }                                                                                                \
-  Name* copy_impl_##Name(const Name* m, bool is_deep) {                                            \
+  static Name* copy_impl_##Name(const Name* m, bool is_deep) {                                     \
     Name* copy    = calloc(1, sizeof(Name));                                                       \
     unsigned size = length_##Name##Table(m->table);                                                \
     copy->table   = new_##Name##Table(size);                                                       \
@@ -68,17 +79,17 @@
   }                                                                                                \
   Name* shallow_copy_##Name(const Name* m) { return copy_impl_##Name(m, false); }                  \
   Name* copy_##Name(const Name* m) { return copy_impl_##Name(m, true); }                           \
-  static Name##Entry make_entry_##Name(const char* k, T v) {                                       \
-    unsigned hash = hash_string(k);                                                                \
-    Name##Entry e;                                                                                 \
-    e.hash  = hash;                                                                                \
-    e.value = v;                                                                                   \
-    e.key   = strdup(k);                                                                           \
+  static Name##Entry* make_entry_##Name(const char* k, T v) {                                      \
+    unsigned hash  = hash_string(k);                                                               \
+    Name##Entry* e = new_##Name##Entry();                                                          \
+    e->hash        = hash;                                                                         \
+    e->value       = v;                                                                            \
+    e->key         = strdup(k);                                                                    \
     return e;                                                                                      \
   }                                                                                                \
   void insert_##Name(Name* m, const char* k, T v) {                                                \
-    Name##Entry e          = make_entry_##Name(k, v);                                              \
-    unsigned idx           = e.hash % length_##Name##Table(m->table);                              \
+    Name##Entry* e         = make_entry_##Name(k, v);                                              \
+    unsigned idx           = e->hash % length_##Name##Table(m->table);                             \
     Name##Entries* es      = get_##Name##Table(m->table, idx);                                     \
     Name##Entries* chained = cons_##Name##Entries(e, es);                                          \
     set_##Name##Table(m->table, idx, chained);                                                     \
@@ -94,10 +105,10 @@
     if (is_nil_##Name##Entries(es)) {                                                              \
       return false;                                                                                \
     }                                                                                              \
-    Name##Entry e = head_##Name##Entries(es);                                                      \
-    if (e.hash == hash) {                                                                          \
+    Name##Entry* e = head_##Name##Entries(es);                                                     \
+    if (e->hash == hash) {                                                                         \
       if (out != NULL) {                                                                           \
-        *out = e.value;                                                                            \
+        *out = e->value;                                                                           \
       }                                                                                            \
       return true;                                                                                 \
     }                                                                                              \
@@ -114,9 +125,9 @@
     if (is_nil_##Name##Entries(es)) {                                                              \
       return;                                                                                      \
     }                                                                                              \
-    Name##Entry e    = head_##Name##Entries(es);                                                   \
+    Name##Entry* e   = head_##Name##Entries(es);                                                   \
     Name##Entries* t = tail_##Name##Entries(es);                                                   \
-    if (e.hash == hash) {                                                                          \
+    if (e->hash == hash) {                                                                         \
       *es = *t;                                                                                    \
       return;                                                                                      \
     }                                                                                              \
@@ -142,10 +153,10 @@
     if (is_nil_##Name##Entries(es)) {                                                              \
       return;                                                                                      \
     }                                                                                              \
-    Name##Entry e = head_##Name##Entries(es);                                                      \
-    fputs(e.key, p);                                                                               \
+    Name##Entry* e = head_##Name##Entries(es);                                                     \
+    fputs(e->key, p);                                                                              \
     fputs(sep_kv, p);                                                                              \
-    print_T(p, e.value);                                                                           \
+    print_T(p, e->value);                                                                          \
     Name##Entries* t = tail_##Name##Entries(es);                                                   \
     if (!is_nil_##Name##Entries(t)) {                                                              \
       fputs(sep, p);                                                                               \

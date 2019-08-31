c8971ea9cb021651c60b2997774a7098aecfe0de
coord.e <me@coord-e.com>
2019-08-31 18:10:06 +0000

    [WIP] Add: Start implementing dce

--- src/dead_code_elim.c
+++ src/dead_code_elim.c
@@ -1,5 +1,34 @@
 #include "dead_code_elim.h"
 
+static void perform_dce(BitSet* live, IRInst* inst) {}
+static void update_live(BitSet* live, IRInst* inst) {}
+
+static void dead_code_elim_function(Function* f) {
+  BBList* l = f->blocks;
+  while (!is_nil_BBList(l)) {
+    BasicBlock* bb = head_BBList(l);
+    if (bb->dead) {
+      l = tail_BBList(l);
+      continue;
+    }
+
+    BitSet* live = copy_BitSet(bb->live_in);
+    for (unsigned i = 0; i < length_IRInstVec(bb->sorted_insts); i++) {
+      IRInst* inst = get_IRInstVec(bb->sorted_insts, i);
+      perform_dce(live, inst);
+      update_live(live, inst);
+    }
+    assert(equal_to_BitSet(bb->live_out, live));
+
+    l = tail_BBList(l);
+  }
+}
+
 void dead_code_elim(IR* ir) {
-  return;
+  FunctionList* l = ir->functions;
+  while (!is_nil_FunctionList(l)) {
+    Function* f = head_FunctionList(l);
+    dead_code_elim_function(f);
+    l = tail_FunctionList(l);
+  }
 }

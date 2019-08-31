c57d86aa10bc01449cfe726c31a8d7149fd7e2de
coord.e <me@coord-e.com>
2019-08-31 18:22:41 +0000

    Add: Implement dce

--- src/dead_code_elim.c
+++ src/dead_code_elim.c
@@ -1,7 +1,29 @@
 #include "dead_code_elim.h"
 
-static void perform_dce(BitSet* live, IRInst* inst) {}
-static void update_live(BitSet* live, IRInst* inst) {}
+static void perform_dce(BitSet* live, IRInst* inst) {
+  if (inst->rd == NULL) {
+    return;
+  }
+
+  if (get_BitSet(live, inst->rd->virtual)) {
+    return;
+  }
+
+  release_Reg(inst->rd);
+  inst->rd = NULL;
+  resize_RegVec(inst->ras, 0);
+  inst->kind = IR_NOP;
+}
+
+static void update_live(BitSet* live, IRInst* inst) {
+  if (inst->rd != NULL) {
+    set_BitSet(live, inst->rd->virtual, false);
+  }
+  for (unsigned i = 0; i < length_RegVec(inst->ras); i++) {
+    Reg* ra = get_RegVec(inst->ras, i);
+    set_BitSet(live, ra->virtual, true);
+  }
+}
 
 static void dead_code_elim_function(Function* f) {
   BBList* l = f->blocks;
@@ -12,13 +34,13 @@ static void dead_code_elim_function(Function* f) {
       continue;
     }
 
-    BitSet* live = copy_BitSet(bb->live_in);
-    for (unsigned i = 0; i < length_IRInstVec(bb->sorted_insts); i++) {
-      IRInst* inst = get_IRInstVec(bb->sorted_insts, i);
-      perform_dce(live, inst);
+    BitSet* live = copy_BitSet(bb->live_out);
+    for (unsigned i = length_IRInstVec(bb->sorted_insts); i > 0; i--) {
+      IRInst* inst = get_IRInstVec(bb->sorted_insts, i - 1);
       update_live(live, inst);
+      perform_dce(live, inst);
     }
-    assert(equal_to_BitSet(bb->live_out, live));
+    assert(equal_to_BitSet(bb->live_in, live));
 
     l = tail_BBList(l);
   }
